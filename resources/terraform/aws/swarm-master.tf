e1fcb6a6fc8606f71142c4af4e145a33f21850ff
Kirill Maksimov <kirill@orbs.com>
2018-12-02 17:07:15 +0200

    Replace remote-exec with user-data (WIP)

--- resources/terraform/aws/swarm-master.tf
+++ resources/terraform/aws/swarm-master.tf
@@ -4,41 +4,7 @@ resource "aws_instance" "master" {
   security_groups = ["${aws_security_group.swarm.name}"]
   key_name        = "${aws_key_pair.deployer.key_name}"
 
-  provisioner "file" {
-    source      = "bootstrap-master.sh"
-    destination = "/tmp/bootstrap.sh"
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
-  }
-
-  provisioner "file" {
-    source      = "docker-compose.yml"
-    destination = "/home/ubuntu/docker-compose.yml"
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/bootstrap.sh",
-      "/tmp/bootstrap.sh",
-      #"docker stack deploy --compose-file /home/ubuntu/docker-compose.yml helloworld"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
-    }
-  }
+  user_data = "#!/bin/sh\nsudo bash -c \"curl -L https://s3.amazonaws.com/orbs-network-releases/infrastructure/boyar/boyar-9e8a5440065ab1d37ec2038c0e8615c931703467.bin -o /usr/bin/boyar && chmod +x /usr/bin/boyar && apt-get update && apt-get install -y docker.io python-pip && pip install awscli && service docker start\""
 
   tags = {
     Name = "constellation-swarm-master"
