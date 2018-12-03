2ab346db810de08de7100aa9d05fec1c18d08653
Kirill Maksimov <kirill@orbs.com>
2018-12-03 18:10:25 +0200

    Pre-pull master image

--- resources/terraform/aws/swarm-slaves.tf
+++ resources/terraform/aws/swarm-slaves.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "slave" {
   key_name             = "${aws_key_pair.deployer.key_name}"
   iam_instance_profile = "orbs-network"
 
-  user_data = "#!/bin/sh\napt-get update && apt-get install -y docker.io python-pip && pip install awscli && service docker start && while true; do docker swarm join --token $(aws secretsmanager get-secret-value --region ${var.region} --secret-id swarm-token-worker-${var.region} --output text --query SecretString) ${aws_instance.master.private_ip} && break; sleep 15; done"
+  user_data = "#!/bin/sh\napt-get update && apt-get install -y docker.io python-pip && pip install awscli && service docker start && $(aws ecr get-login --no-include-email --region us-west-2) && docker pull 506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network-v1:master && while true; do docker swarm join --token $(aws secretsmanager get-secret-value --region ${var.region} --secret-id swarm-token-worker-${var.region} --output text --query SecretString) ${aws_instance.master.private_ip} && break; sleep 15; done"
 
   tags = {
     Name = "constellation-swarm-worker-${count.index}"
