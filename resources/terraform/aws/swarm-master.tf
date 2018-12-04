34653c705d0f2c8beb9d4be9560756611b4dfe1a
Kirill Maksimov <kirill@orbs.com>
2018-12-04 15:49:26 +0200

    Refactor user data

--- resources/terraform/aws/swarm-master.tf
+++ resources/terraform/aws/swarm-master.tf
@@ -1,3 +1,49 @@
+locals {
+  master_user_data = <<TFEOF
+#!/bin/sh
+
+# Remove old instances of Docker which might ship with ubuntu
+apt-get remove docker docker-engine docker.io
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# Complete fingerprint: 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+apt-get update
+apt-get install -y docker-ce
+
+export BOYAR_VERSION=e44569483b36914d2651ef65cede0730365d161c
+
+curl -L https://s3.amazonaws.com/orbs-network-releases/infrastructure/boyar/boyar-$BOYAR_VERSION.bin -o /usr/bin/boyar && chmod +x /usr/bin/boyar
+
+apt-get install -y python-pip && pip install awscli
+
+docker swarm init
+
+aws secretsmanager create-secret --region ${var.region} --name swarm-token-worker-${var.region} --secret-string $(docker swarm join-token --quiet worker) || aws secretsmanager put-secret-value --region ${var.region} --secret-id swarm-token-worker-${var.region} --secret-string $(docker swarm join-token --quiet worker)
+
+mkdir -p /opt/orbs && mv /tmp/keys.json /opt/orbs
+
+$(aws ecr get-login --no-include-email --region us-west-2)
+
+while true; do
+    [ $(docker node ls -q | wc -l) -eq 3 ] && HOME=/root boyar --config-url https://s3-us-west-2.amazonaws.com/orbs-network-config-staging/discovery/config.json --orchestrator swarm --keys /opt/orbs/keys.json && sleep 15 && break;
+done
+TFEOF
+}
+
 resource "aws_instance" "master" {
   ami                  = "${var.aws_ami_id}"
   instance_type        = "${var.aws_orbs_master_instance_type}"
@@ -5,7 +51,7 @@ resource "aws_instance" "master" {
   key_name             = "${aws_key_pair.deployer.key_name}"
   iam_instance_profile = "orbs-network"
 
-  user_data = "#!/bin/sh\ncurl -L https://s3.amazonaws.com/orbs-network-releases/infrastructure/boyar/boyar-9e8a5440065ab1d37ec2038c0e8615c931703467.bin -o /usr/bin/boyar && chmod +x /usr/bin/boyar && apt-get update && apt-get install -y docker.io python-pip && pip install awscli && service docker start && docker swarm init &&\naws secretsmanager create-secret --region ${var.region} --name swarm-token-worker-${var.region} --secret-string $(docker swarm join-token --quiet worker) || aws secretsmanager put-secret-value --region ${var.region} --secret-id swarm-token-worker-${var.region} --secret-string $(docker swarm join-token --quiet worker) && mkdir -p /opt/orbs && mv /tmp/keys.json /opt/orbs && $(aws ecr get-login --no-include-email --region us-west-2) && docker pull 506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network-v1:master && while true; do [ $(docker node ls -q | wc -l) -eq 3 ] && boyar --config-url https://s3-us-west-2.amazonaws.com/orbs-network-config-staging/discovery/config.json --orchestrator swarm --keys /opt/orbs/keys.json && sleep 15 && break; done"
+  user_data = "${local.master_user_data}"
 
   tags = {
     Name = "constellation-swarm-master"
--- resources/terraform/aws/swarm-slaves.tf
+++ resources/terraform/aws/swarm-slaves.tf
@@ -1,3 +1,39 @@
+locals {
+  slave_user_data = <<TFEOF
+#!/bin/sh
+
+# Remove old instances of Docker which might ship with ubuntu
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Complete fingerprint: 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+apt-get install -y python-pip && pip install awscli
+
+while true; do
+    docker swarm join --token $(aws secretsmanager get-secret-value --region ${var.region} --secret-id swarm-token-worker-${var.region} --output text --query SecretString) ${aws_instance.master.private_ip} && break
+    sleep 15
+done
+
+TFEOF
+}
+
 resource "aws_instance" "slave" {
   count                = 2
   ami                  = "${var.aws_ami_id}"
@@ -6,7 +42,7 @@ resource "aws_instance" "slave" {
   key_name             = "${aws_key_pair.deployer.key_name}"
   iam_instance_profile = "orbs-network"
 
-  user_data = "#!/bin/sh\napt-get update && apt-get install -y docker.io python-pip && pip install awscli && service docker start && $(aws ecr get-login --no-include-email --region us-west-2) && docker pull 506367651493.dkr.ecr.us-west-2.amazonaws.com/orbs-network-v1:master && while true; do docker swarm join --token $(aws secretsmanager get-secret-value --region ${var.region} --secret-id swarm-token-worker-${var.region} --output text --query SecretString) ${aws_instance.master.private_ip} && break; sleep 15; done"
+  user_data = "${local.slave_user_data}"
 
   tags = {
     Name = "constellation-swarm-worker-${count.index}"
