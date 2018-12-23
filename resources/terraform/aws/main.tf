211646852a0a33ff9dfdab5a49f89f1a387815d1
Kirill Maksimov <kirill@orbs.com>
2018-12-23 14:00:19 +0200

    Replace Sydney with Sao Paolo

--- resources/terraform/aws/main.tf
+++ resources/terraform/aws/main.tf
@@ -7,7 +7,6 @@ module "vpc" {
   application    = "${ var.application }"
   provisionersrc = "${ var.provisionersrc }"
 
-  # because australia doesn't have t3.medium in the a zone
-  azs  = "${ slice(data.aws_availability_zones.available.names, 1, length(data.aws_availability_zones.available.names) - 1) }"
+  azs  = "${ data.aws_availability_zones.available.names }"
   cidr = "${ var.vpc_cidr_block }"
 }
