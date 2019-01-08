275b7d6124c82e7a8d40445769b45ad11fd62795
itamar Arjuan <itamar.arjuan@gmail.com>
2019-01-08 16:26:46 +0200

    script to install terraform

--- build/setup.sh
+++ build/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get install unzip
+wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
+unzip terraform_0.11.10_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform --version 
