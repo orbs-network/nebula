528974113a7ebfe2e69116c137c37dd7b56249bd
Kirill Maksimov <kirill@orbs.com>
2019-02-14 17:20:08 +0200

    Add more tests for metrics utility functions

--- test/unit/metrics-tests.js
+++ test/unit/metrics-tests.js
@@ -1,31 +1,78 @@
-const { describe, it } = require("mocha");
+const nock = require("nock");
+const {
+    describe,
+    it
+} = require("mocha");
 const chai = require("chai");
 const asserttype = require("chai-asserttype");
 chai.use(asserttype);
 
-const { expect } = chai;
+const {
+    expect
+} = chai;
 
-const { getMetrics, getBlockHeight, waitUntil, waitUntilSync } = require("./../../lib/metrics.js");
+const {
+    getMetrics,
+    getBlockHeight,
+    getVersion,
+    waitUntil,
+    waitUntilSync,
+    waitUntilVersion
+} = require("./../../lib/metrics.js");
 
-const ENDPOINT_STAGING = "us-east-1.global.nodes.staging.orbs-test.com/vchains/2013";
+const HOST = "fake.staging.orbs-test.com";
+const PATH = "/vchains/2013";
+const ENDPOINT_STAGING = `${HOST}${PATH}`;
+
+function mockMetricsResponse() {
+    const response = require(`${__dirname}/metrics.json`);
+
+    nock.disableNetConnect();
+
+    nock(`http://${HOST}`)
+    .persist()
+    .get(`${PATH}/metrics`)
+    .reply(200, response);
+}
 
 describe.only("metrics unit tests", () => {
+    before(() => {
+        mockMetricsResponse()
+    });
+
     it("should retrive metrics", async () => {
         const blockHeight = await getMetrics(ENDPOINT_STAGING);
         expect(blockHeight).not.to.be.empty;
     });
 
-    it("should retrive block height", async () => {
+    it("should retrieve block height", async () => {
         const blockHeight = await getBlockHeight(ENDPOINT_STAGING);
         expect(blockHeight).be.greaterThan(0);
     });
-    
+
+    it("should retrieve version", async () => {
+        const version = await getVersion(ENDPOINT_STAGING);
+        expect(version).to.include("83a149e417")
+    })
+
     describe("#waitUntil", () => {
         it("should wait until event occures", async () => {
             let i = 0;
-            
+
             await waitUntil(3, async () => {
-                return i+=1;
+                return i += 1;
+            }, 2);
+
+            expect(i).to.be.eql(3);
+        });
+
+        it("accepts test function", async () => {
+            let i = 0;
+
+            await waitUntil((value) => {
+                return value % 3 == 0;
+            }, async () => {
+                return i += 1;
             }, 2);
 
             expect(i).to.be.eql(3);
@@ -33,9 +80,9 @@ describe.only("metrics unit tests", () => {
 
         it("should return duration of the operation", async () => {
             let i = 0;
-            
+
             const duration = await waitUntil(3, async () => {
-                return i+=1;
+                return i += 1;
             }, 2);
 
             expect(duration).to.be.greaterThan(0);
@@ -66,6 +113,15 @@ describe.only("metrics unit tests", () => {
 
             console.log(await getBlockHeight(ENDPOINT_STAGING));
             expect(await getBlockHeight(ENDPOINT_STAGING)).to.be.gte(1000);
+        });
+    });
+
+    describe("#waitUntilVersion", () => {
+        it("test against staging", async () => {
+            await waitUntilVersion(ENDPOINT_STAGING, "83a149e4");
+
+            console.log(await getVersion(ENDPOINT_STAGING));
+            expect(await getVersion(ENDPOINT_STAGING)).to.include("83a149e4");
         });
     });
 });
