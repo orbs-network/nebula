f480a44ff7965255fba7b4dba482844a772dd960
Kirill Maksimov <kirill@orbs.com>
2019-03-21 20:45:26 +0200

    Bump number of states from 2000 to 5000

--- resources/terraform/aws/ethereum.tf
+++ resources/terraform/aws/ethereum.tf
@@ -48,7 +48,7 @@ docker run -d \
   --base-path /home/parity/data \
   --min-peers=45 --max-peers=60 \
   --no-secretstore --jsonrpc-interface all --no-ui --no-ipc --no-ws \
-  --pruning-history 2000
+  --pruning-history 5000
 
 TFEOF
 }
