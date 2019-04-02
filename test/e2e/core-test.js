3c29794452cace98e731e2949a2fbce4b72f4297
itamar Arjuan <itamar.arjuan@gmail.com>
2019-04-02 13:28:29 +0300

    removing the testnet folder and limiting the tests for the core only for now.

--- test/e2e/core-test.js
+++ test/e2e/core-test.js
@@ -16,7 +16,7 @@ const c = new CoreService(new TerraformService(terraformProdAdapter, cachePathFo
 const region = 'sa-east-1';
 let preExistingElasticIp;
 
-describe('nebula core api', () => {
+describe.only('nebula core api', () => {
     before(async () => {
         // First we will create an Elastic IP outside the scope of createConstellation()
         console.log('Allocating a public IP from AWS...');
