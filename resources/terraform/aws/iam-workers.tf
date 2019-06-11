b5f88ff8c0ac68efcd43e176fd6a0b46790b97a9
Kirill Maksimov <kirill@orbs.com>
2019-06-11 18:22:07 +0300

    Split worker iam policy into multiple

--- resources/terraform/aws/iam-workers.tf
+++ resources/terraform/aws/iam-workers.tf
@@ -1,39 +1,7 @@
 # IAM Role resources for swarm workers
-resource "aws_iam_role" "swarm_worker" {
-  name               = "orbs-constellation-${var.name}-worker"
-  assume_role_policy = "${data.aws_iam_policy_document.swarm_role.json}"
-}
-
-data "aws_iam_policy_document" "swarm_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_policy" "swarm_worker" {
-  name   = "orbs-constellation-${var.name}-worker-policy"
-  path   = "/"
-  policy = "${data.aws_iam_policy_document.swarm_worker.json}"
-}
-
-data "aws_iam_policy_document" "swarm_worker" {
+data "aws_iam_policy_document" "swarm_worker_ebs" {
   statement {
     actions = [
-      "secretsmanager:GetSecretValue",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "secretsmanager:GetSecretValue",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
       "ec2:AttachVolume",
       "ec2:CreateVolume",
       "ec2:CreateSnapshot",
@@ -58,10 +26,84 @@ data "aws_iam_policy_document" "swarm_worker" {
   }
 }
 
-resource "aws_iam_policy_attachment" "swarm_worker" {
-  name       = "swarm-worker-${var.name}-iam-policy-attachment"
+data "aws_iam_policy_document" "swarm_worker_ecr" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "swarm_worker_secrets" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+
+resource "aws_iam_role" "swarm_worker" {
+  name               = "orbs-constellation-${var.name}-worker"
+  assume_role_policy = "${data.aws_iam_policy_document.swarm_role.json}"
+}
+
+data "aws_iam_policy_document" "swarm_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "swarm_worker_ebs" {
+  name   = "orbs-constellation-${var.name}-ebs-worker-policy"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.swarm_worker_ebs.json}"
+}
+
+resource "aws_iam_policy" "swarm_worker_ecr" {
+  name   = "orbs-constellation-${var.name}-ecr-worker-policy"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.swarm_worker_ecr.json}"
+}
+
+resource "aws_iam_policy" "swarm_worker_secrets" {
+  name   = "orbs-constellation-${var.name}-secrets-worker-policy"
+  path   = "/"
+  policy = "${data.aws_iam_policy_document.swarm_worker_secrets.json}"
+}
+
+resource "aws_iam_policy_attachment" "swarm_worker_ebs" {
+  name       = "swarm-worker-${var.name}-ebs-iam-policy-attachment"
   roles      = ["${aws_iam_role.swarm_worker.name}"]
-  policy_arn = "${aws_iam_policy.swarm_worker.arn}"
+  policy_arn = "${aws_iam_policy.swarm_worker_ebs.arn}"
+}
+
+resource "aws_iam_policy_attachment" "swarm_worker_ecr" {
+  name       = "swarm-worker-${var.name}-ecr-iam-policy-attachment"
+  roles      = ["${aws_iam_role.swarm_worker.name}"]
+  policy_arn = "${aws_iam_policy.swarm_worker_ecr.arn}"
+}
+
+resource "aws_iam_policy_attachment" "swarm_worker_secrets" {
+  name       = "swarm-worker-${var.name}-secrets-iam-policy-attachment"
+  roles      = ["${aws_iam_role.swarm_worker.name}"]
+  policy_arn = "${aws_iam_policy.swarm_worker_secrets.arn}"
 }
 
 resource "aws_iam_instance_profile" "swarm_worker" {
