a05c41e80ee550130e92fa24dadc3c27975614ce
itamar Arjuan <itamar.arjuan@gmail.com>
2020-01-15 14:33:06 +0200

    removing the testkit from this branch

--- docker/orbs-network-testkit/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo "Building Docker image for orbs-network-go testkit"
-
-docker build -t itamararjuan/orbs-network-go:testkit .
-
-docker push itamararjuan/orbs-network-go:testkit
