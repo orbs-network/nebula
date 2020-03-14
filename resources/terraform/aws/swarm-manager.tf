26f49cce2f0ab868abaf510bfc1b5a42a5a1bb49
Kirill Maksimov <kirill@orbs.com>
2020-03-14 06:48:25 +0200

    Management service (WIP)

--- resources/terraform/aws/swarm-manager.tf
+++ resources/terraform/aws/swarm-manager.tf
@@ -114,14 +114,21 @@ if [ ! -z "$(cat $SSL_CERT_PATH)" ] && [ ! -z "$(cat $SSL_PRIVATE_KEY_PATH)" ]; 
   export SSL_PARAMS="--ssl-certificate $SSL_CERT_PATH --ssl-private-key $SSL_PRIVATE_KEY_PATH"
 fi
 
+export MANAGEMENT_CONFIG_PATH=/opt/orbs/management-config.json
+echo <<-EOF
+${var.boyar_management_config}
+EOF > $MANAGEMENT_CONFIG_PATH
+
+export MANAGEMENT_CONFIG_PARAMS="${var.boyar_management_config == "" ? "" : "--management-config $MANAGEMENT_CONFIG_PATH"}"
+
 # Install supervisord to keep Boyar alive even after a restart to the EC2 instance
 apt-get install -y supervisor tar
 
 echo "[program:boyar]
-command=/usr/bin/boyar --logger-http-endpoint \"${var.logz_io_http_endpoint}\" --config-url ${var.s3_boyar_config_url} --keys /opt/orbs/keys.json --daemonize --max-reload-time-delay 0m $ETHEREUM_PARAMS $SSL_PARAMS
+command=/usr/bin/boyar --logger-http-endpoint \"${var.logz_io_http_endpoint}\" --config-url ${var.s3_boyar_config_url} --keys /opt/orbs/keys.json --daemonize --max-reload-time-delay 0m $ETHEREUM_PARAMS $SSL_PARAMS $MANAGEMENT_CONFIG_PARAMS
 autostart=true
 autorestart=true
-environment=HOME=\"/root\", ETHEREUM_PARAMS=\"$ETHEREUM_PARAMS\", SSL_PARAMS=\"$SSL_PARAMS\"
+environment=HOME=\"/root\", ETHEREUM_PARAMS=\"$ETHEREUM_PARAMS\", SSL_PARAMS=\"$SSL_PARAMS\", MANAGEMENT_CONFIG_PARAMS="$MANAGEMENT_CONFIG_PARAMS"
 stderr_logfile=/var/log/boyar.err.log
 stdout_logfile=/var/log/boyar.log" >> /etc/supervisor/conf.d/boyar.conf
 
--- resources/terraform/aws/variables.tf
+++ resources/terraform/aws/variables.tf
@@ -63,6 +63,10 @@ variable "boyar_version" {
   default = "v0.22.0"
 }
 
+variable "boyar_management_config" {
+  default = ""
+}
+
 variable "incoming_ssh_cidr_blocks" {
   default = []
   type = "list"
