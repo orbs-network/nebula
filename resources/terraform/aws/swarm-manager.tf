3c86fae05878446c90349fd1ebd1511a33cbe8bb
Kirill Maksimov <kirill@orbs.com>
2020-07-15 14:27:49 +0300

    Integrate boyar logs and status

--- resources/terraform/aws/swarm-manager.tf
+++ resources/terraform/aws/swarm-manager.tf
@@ -129,15 +129,17 @@ export MANAGEMENT_CONFIG_PARAMS="${var.boyar_management_config == "" ? "" : "--m
 # Install supervisord to keep Boyar alive even after a restart to the EC2 instance
 apt-get install -y supervisor tar daemontools
 
-mkdir -p /var/efs/boyar/
-mkdir -p /var/efs/signer-logs/
+mkdir -p /var/efs/boyar-logs/
+mkdir -p /var/efs/boyar-status/
+
+ln -s /var/efs/boyar-logs/current /var/log/boyar.log
 
 echo "[program:boyar]
-command=/usr/bin/boyar --config-url ${var.s3_boyar_config_url} --keys /opt/orbs/keys.json --max-reload-time-delay 0m $ETHEREUM_PARAMS $SSL_PARAMS $MANAGEMENT_CONFIG_PARAMS
+command=/usr/bin/boyar --config-url ${var.s3_boyar_config_url} --keys /opt/orbs/keys.json --max-reload-time-delay 0m --status /var/efs/boyar-status/status.json $ETHEREUM_PARAMS $SSL_PARAMS $MANAGEMENT_CONFIG_PARAMS
 autostart=true
 autorestart=true
 environment=HOME=\"/root\", ETHEREUM_PARAMS=\"$ETHEREUM_PARAMS\", SSL_PARAMS=\"$SSL_PARAMS\", MANAGEMENT_CONFIG_PARAMS=\"$MANAGEMENT_CONFIG_PARAMS\"
-stdout_logfile=/var/efs/boyar/current
+stdout_logfile=/var/efs/boyar-logs/current
 redirect_stderr=true
 stdout_logfile_maxbytes=10MB" >> /etc/supervisor/conf.d/boyar.conf
 
